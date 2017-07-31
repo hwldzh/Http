1c84e83184860a0bf5c95d5e6ee8698f055d48f4
hwldzh <821187649@qq.com>
2017-07-31 22:18:09 +0800

    提交

--- app/src/main/java/com/example/runningh/testhttp/MainActivity.java
+++ app/src/main/java/com/example/runningh/testhttp/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.runningh.testhttp;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        get();
+    }
+
+    private void get() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url("http://www.baidu.com").get().build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
     }
 }
